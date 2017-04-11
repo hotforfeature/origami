2f3f6a0b5ac02dbd1ce109dea4b2fa3870f8fddc
Andrew Mitchell <abmitchell77@gmail.com>
2017-04-11 16:52:19 -0500

    fix(collections): missing paper-tab selector

--- src/collections/paper-elements/paper-ui-elements.ts
+++ src/collections/paper-elements/paper-ui-elements.ts
@@ -5,6 +5,6 @@ import { PolymerDirective } from '../../events/polymer.directive';
 @Directive({
   selector: `paper-badge, paper-button, paper-card, paper-dropdown-menu, paper-icon-button,
     paper-icon-item, paper-item, paper-item-body, paper-menu, paper-menu-button, paper-progress,
-    paper-spinner, paper-swatch-picker, paper-tabs, paper-toolbar, paper-tooltip`
+    paper-spinner, paper-swatch-picker, paper-tab, paper-tabs, paper-toolbar, paper-tooltip`
 })
 export class PaperUiElement extends PolymerDirective { }
