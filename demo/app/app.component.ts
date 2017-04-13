bd2186b90133de3cd6558a2684589dd7b09bee09
Andrew Mitchell <abmitchell77@gmail.com>
2017-04-13 16:17:12 -0500

    chore(demo): fix style issues with ShadyCSS for Safari/IE

--- demo/app/app.component.ts
+++ demo/app/app.component.ts
@@ -1,29 +1,19 @@
-import { Component, ElementRef, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 import { CustomStyleService, PolymerProperty } from '../origami/origami';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styles and styleUrls both work
   styleUrls: ['./app.component.css'],
-  /*styles: [
-    `app-toolbar {
-      @apply --shadow-elevation-4dp;
-
-      background: var(--primary-color);
-      color: var(--dark-theme-text-color);
-    }`
-  ],*/
-  // encapsulation: ViewEncapsulation.Native
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
   @PolymerProperty() selectedTab: number = 1;
 
-  constructor(private elementRef: ElementRef, private customStyle: CustomStyleService) { }
+  constructor(private customStyle: CustomStyleService) { }
 
   ngOnInit() {
-    // this.elementRef is only required with ViewEncapsulation.Native
-    this.customStyle.updateCustomStyles(this.elementRef);
+    this.customStyle.updateCustomStyles();
   }
 }
