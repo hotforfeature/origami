507ac23ffebcc480b5753d7638dc7ecded280634
Andrew Mitchell <abmitchell77@gmail.com>
2017-04-21 16:16:06 -0500

    fix(events): fix bug where value for decorator was cached across all components

--- src/events/polymer-changes.ts
+++ src/events/polymer-changes.ts
@@ -3,36 +3,44 @@ import { OnPolymerChange } from './on-polymer-change';
 export function PolymerChanges(): PropertyDecorator {
   return (target: any, propertyKey: string) => {
     const desc = Object.getOwnPropertyDescriptor(target, propertyKey);
+    /* istanbul ignore if */
     if (desc && desc.get && !desc.set) {
       console.warn(`${propertyKey} is readonly. @PolymerChanges() is not needed`);
     }
 
-    let value: any;
+    const properties = new WeakMap();
     return {
       configurable: desc ? desc.configurable : true,
       enumerable: desc ? desc.enumerable : true,
       get() {
         if (desc && desc.get) {
-          return desc.get();
+          return desc.get.apply(this);
         } else {
-          return value;
+          const props = properties.get(this);
+          return props && props[propertyKey];
         }
       },
       set(event: any|CustomEvent) {
         if (event instanceof CustomEvent && event.detail.path) {
           // Object or Array mutation, we need to tell Angular that things have changed
-          if ((<OnPolymerChange>this).onPolymerChange && event instanceof CustomEvent) {
-            (<OnPolymerChange>this).onPolymerChange(propertyKey, value, event.detail);
+          if ((<OnPolymerChange>this).onPolymerChange) {
+            (<OnPolymerChange>this).onPolymerChange(propertyKey, event, event.detail);
           }
         } else {
-          let newValue = unwrapPolymerEvent(event);
-          if (desc && desc.set) {
-            desc.set(newValue);
+          let props = properties.get(this);
+          if (!props) {
+            props = {};
+            properties.set(this, props);
           }
 
-          if (newValue !== value) {
+          let newValue = unwrapPolymerEvent(event);
+          if (desc && desc.set) {
+            desc.set.apply(this, [newValue]);
+          }
+
+          if (newValue !== props[propertyKey]) {
             // Even if there is a setter, we still keep a copy to determine if a change happens
-            value = newValue;
+            props[propertyKey] = newValue;
 
             if ((<OnPolymerChange>this).onPolymerChange && event instanceof CustomEvent) {
               (<OnPolymerChange>this).onPolymerChange(propertyKey, event, event.detail);
