545643b2eebd9ec431a43c5438e5b667fc73499b
Andrew Mitchell <abmitchell77@gmail.com>
2017-06-12 08:41:32 -0500

    chore(demo): add vaadin elements kitchen sink with vaadin-grid
    #18

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { PolymerChanges } from '@codebakery/origami';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  @PolymerChanges() selectedTab: number = 1;
+  @PolymerChanges() selectedTab: number = 0;
   @PolymerChanges() model: string;
 }
