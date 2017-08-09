e04d8ef44e3094557ac4092b34859eb342e09ab7
Andrew Mitchell <abmitchell77@gmail.com>
2017-08-09 15:56:06 -0500

    fix(collections): add paper-toast to PaperElementsModule

--- origami/collections/src/paper-elements/paper-ui-elements.ts
+++ origami/collections/src/paper-elements/paper-ui-elements.ts
@@ -4,6 +4,7 @@ import { EmitChangesDirective } from '@codebakery/origami';
 @Directive({
   selector: `paper-badge, paper-button, paper-card, paper-dropdown-menu, paper-icon-button,
     paper-icon-item, paper-item, paper-item-body, paper-menu, paper-menu-button, paper-progress,
-    paper-spinner, paper-swatch-picker, paper-tab, paper-tabs, paper-toolbar, paper-tooltip`
+    paper-spinner, paper-swatch-picker, paper-tab, paper-tabs, paper-toast, paper-toolbar,
+    paper-tooltip`
 })
 export class PaperUiElement extends EmitChangesDirective { }
